e3d63c9d6a9ba200d153e9b4f36441f31357a887
Derek Van Hove <d.vanhove@nukama.be>
2015-01-23 12:06:33 +0100

    Created worker.c
    Signed-off-by: Derek Van Hove <d.vanhove@nukama.be>

---
+++ quarantine/worker.c
@@ -0,0 +1,9 @@
+/*
+ * Process that associates tasks with required quarantine actions
+ * Forked from Klearnel, forks to accomplish certain tasks
+ * 
+ * Copyright (C) 2014, 2015 Klearnel-Devs
+ */
+#include <global.h>
+#include <quarantine/quarantine.h>
+
