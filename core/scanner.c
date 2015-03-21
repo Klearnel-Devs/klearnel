9d636752ae0d9762b6e7745e2013686d63ff6d50
Antoine Ceyssens <a.ceyssens@nukama.be>
2015-03-21 15:01:21 +0100

    Save function implemented
    Signed-off-by: Antoine Ceyssens <a.ceyssens@nukama.be>

--- core/scanner.c
+++ core/scanner.c
@@ -8,7 +8,7 @@
 #include <logging/logging.h>
 #include <core/scanner.h>
 
-static TWatchElementList* elem_list = NULL;
+static TWatchElementList* watch_list = NULL;
 
 
 int init_scanner()
@@ -31,42 +31,42 @@ int add_watch_elem(TWatchElement elem)
 	}
 	node->element = elem;
 	node->next = NULL;
-	if (!elem_list) {
-		elem_list = malloc(sizeof(struct watchElementList));
-		if (!elem_list) {
+	if (!watch_list) {
+		watch_list = malloc(sizeof(struct watchElementList));
+		if (!watch_list) {
 			LOG(FATAL, "Unable to allocate memory");
 			return -1;
 		}
 		node->prev = NULL;
-		elem_list->first = node;
-		elem_list->last = node;
-		elem_list->count = 1;
+		watch_list->first = node;
+		watch_list->last = node;
+		watch_list->count = 1;
 		return 0;
 	}
 	
-	node->prev = elem_list->last;
-	elem_list->last->next = node;
-	elem_list->last = node;
-	elem_list->count++;
+	node->prev = watch_list->last;
+	watch_list->last->next = node;
+	watch_list->last = node;
+	watch_list->count++;
 	return 0;
 }
 
 int remove_watch_elem(TWatchElement elem) 
 {
-	TWatchElementNode* item = elem_list->first;
+	TWatchElementNode* item = watch_list->first;
 	if (!item) return 0;
 	while (item) {
 		if (strcmp(item->element.path, elem.path) == 0) {
-			if (strcmp(elem_list->first->element.path, item->element.path) == 0) {
-				elem_list->first = item->next;
+			if (strcmp(watch_list->first->element.path, item->element.path) == 0) {
+				watch_list->first = item->next;
 			}
-			if (strcmp(elem_list->last->element.path, item->element.path) == 0) {
-				elem_list->last = item->prev;
+			if (strcmp(watch_list->last->element.path, item->element.path) == 0) {
+				watch_list->last = item->prev;
 			}
 			if (item->next) item->next->prev = item->prev;
 			if (item->prev)	item->prev->next = item->next;
 			free(item);
-			elem_list->count--;
+			watch_list->count--;
 			return 0;
 		}
 	}
@@ -98,3 +98,23 @@ int load_watch_list()
 	return 0;
 }
 
+int save_watch_list()
+{
+	int fd, i;
+
+	if ((fd = open(SCAN_DB, O_WRONLY)) < 0) {
+		LOG(URGENT, "Unable to open the SCAN_DB");
+		return -1;
+	}
+	TWatchElementNode* item = watch_list->first;
+	for(i = 0; i < watch_list->count; i++) {
+		if (write(fd, item->element, sizeof(struct watchElement)) < 0) {
+			write_to_log(FATAL, "%s:%s: Unable to write \"%s\" in SCAN_DB", 
+				__func__, __LINE__, item->element.path);
+			return -1;
+		}
+	}
+	free(watch_list);
+	close(fd);
+	return 0;
+}
