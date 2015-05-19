2c772c94d28c31225e02e2529c50aa702f1d8b36
Antoine Ceyssens <a.ceyssens@nukama.be>
2015-05-19 18:15:22 +0200

    Changed error output to log file for networker.c
    Signed-off-by: Antoine Ceyssens <a.ceyssens@nukama.be>

--- net/networker.c
+++ net/networker.c
@@ -27,7 +27,7 @@ int _execute_qr_action(const char *buf, const int c_len, const int action, const
 	timeout.tv_usec	= 0;
 
 	if ((s_cl = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-		perror("[UI] Unable to create socket");
+		LOG(FATAL, "Unable to create socket");
 		return -1;
 	}
 
@@ -36,20 +36,20 @@ int _execute_qr_action(const char *buf, const int c_len, const int action, const
 	len = strlen(remote.sun_path) + sizeof(remote.sun_family);
 
 	if (connect(s_cl, (struct sockaddr *)&remote, len) == -1) {
-		perror("[UI] Unable to connect the qr_sock");
+		LOG(FATAL, "Unable to connect the qr_sock");
 		goto error;
 	}
 	if (setsockopt(s_cl, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,	sizeof(timeout)) < 0)
-		perror("[UI] Unable to set timeout for reception operations");
+		LOG(WARNING, "Unable to set timeout for reception operations");
 
 	if (setsockopt(s_cl, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
-		perror("[UI] Unable to set timeout for sending operations");
+		LOG(WARNING, "Unable to set timeout for sending operations");
 
 	len = 20;
 	res = malloc(2);
 	query = malloc(len);
 	if ((query == NULL) || (res == NULL)) {
-		perror("[UI] Unable to allocate memory");
+		LOG(FATAL, "Unable to allocate memory");
 		goto error;
 	}
 	switch (action) {
@@ -63,40 +63,40 @@ int _execute_qr_action(const char *buf, const int c_len, const int action, const
 		case QR_REST: ;
 			snprintf(query, len, "%d:%d", action, c_len);
 			if (write(s_cl, query, len) < 0) {
-				perror("[UI] Unable to send query");
+				LOG(URGENT, "Unable to send query");
 				goto error;
 			}
 			if (read(s_cl, res, 2) < 0) {
-				perror("[UI] Unable to get query result");
+				LOG(URGENT, "Unable to get query result");
 				goto error;
 			}
-			
+
 			if (write(s_cl, buf, c_len) < 0) {
-				perror("[UI] Unable to send args of the query");
+				LOG(URGENT, "Unable to send args of the query");
 				goto error;
 			}
 			if (read(s_cl, res, 2) < 0) {
-				perror("[UI] Unable to get query result");
+				LOG(URGENT, "Unable to get query result");
 				goto error;					
 			}
 			if (read(s_cl, res, 2) < 0) {
-				perror("[UI] Unable to get query result");
+				LOG(URGENT, "Unable to get query result");
 				goto error;
 			}
 			if (!strcmp(res, SOCK_ACK)) {
 				switch(action) {
-					case QR_ADD:  printf("File %s successfully added to QR\n", buf); break;
-					case QR_RM:   printf("File %s successfully deleted from QR\n", buf); break;
-					case QR_REST: printf("File %s successfully restored\n", buf); break;
+					case QR_ADD:  write_to_log(INFO, "File %s successfully added to QR\n", buf); break;
+					case QR_RM:   write_to_log(INFO, "File %s successfully deleted from QR\n", buf); break;
+					case QR_REST: write_to_log(INFO, "File %s successfully restored\n", buf); break;
 				}
 			} else if (!strcmp(res, SOCK_ABORTED)) {
 				switch(action) {
-					case QR_ADD:  printf("File %s could not be added to QR\n", buf); break;
-					case QR_RM:   printf("File %s could not be deleted from QR\n", buf); break;
-					case QR_REST: printf("File %s could not be restored\n", buf); break;
+					case QR_ADD:  write_to_log(INFO, "File %s could not be added to QR\n", buf); break;
+					case QR_RM:   write_to_log(INFO, "File %s could not be deleted from QR\n", buf); break;
+					case QR_REST: write_to_log(INFO, "File %s could not be restored\n", buf); break;
 				}
 			} else if (!strcmp(res, SOCK_UNK)) {
-				fprintf(stderr, "File not found in quarantine\n");
+				LOG(WARNING, "File not found in quarantine");
 			}		
 			break;
 
